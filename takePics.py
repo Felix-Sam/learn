7f60558da4eeca77ea82cf0d98a55aea69313e08
Felixsam <felixsam922@gmail.com>
2023-08-16 10:23:36 -0700

    version one one

--- takePics.py
+++ takePics.py
@@ -3,13 +3,13 @@ import cv2
 import time
 cap = cv2.VideoCapture(0)
 
-num = 0
+
 for i in range(10):
     ret,image = cap.read()
     time.sleep(3)
-    path = os.path.join('images', f'image{num}.jpg')
+    path = os.path.join('images', f'image{i}.jpg')
     cv2.imwrite(path,image)
-    num += 1
+
     cv2.imshow('frame',image)
     cv2.waitKey(1)
 cv2.destroyAllWindows()
